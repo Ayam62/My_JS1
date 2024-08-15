c1b8caa571c84b1f643197903fe2f44c9a38e76a
Ayam62 <153174024+Ayam62@users.noreply.github.com>
2024-08-15 12:03:53 +0000

    datatypes

---
+++ 01_basics/01dataTypes.js
@@ -0,0 +1,5 @@
+var name="ayam"
+var age=19
+var gender="male"
+console.table([typeof name,typeof age,typeof "male"])
+
---
+++ 01_basics/01variable.js
@@ -0,0 +1,8 @@
+const accountId="9863718782"
+let accountName="AyamKattel"
+var accountCode="9863"
+accountCity="Lalitpur"
+
+console.table([accountId,accountName,accountCode,9844156600])
+//console.log(accountId)
+
