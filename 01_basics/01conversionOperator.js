f54e6e9ed6c66277ba56b6d3cbeedf3358daedda
Ayam62 <153174024+Ayam62@users.noreply.github.com>
2024-08-15 12:21:08 +0000

    conversion Of Datatype

---
+++ 01_basics/01conversionOperator.js
@@ -0,0 +1,13 @@
+let score="33abc"
+console.log(typeof score)
+let value=Number(score)
+console.log(typeof value)
+console.log(value)
+//"55"=>55
+//"55abc"=>NaN
+//true=>1
+let number=1
+let bool1=Boolean(number)
+console.log(typeof bool1)
+console.log(bool1)
+//1=>true
