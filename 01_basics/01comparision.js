bf4833d935a7bfa00f71b4d92a163d4923bacccc
Ayam62 <153174024+Ayam62@users.noreply.github.com>
2024-08-15 12:46:59 +0000

    comparisions

---
+++ 01_basics/01comparision.js
@@ -0,0 +1,6 @@
+console.log(null >0)
+console.log(null==0)
+console.log(null>= 0)
+console.log(undefined >=0)
+console.log("2"==2)//=>true because it checks only the value not datatype
+console.log("2"===2)//=>false because it checks the data type too
