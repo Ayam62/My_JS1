5c6cbf54392d74355b0898e6754977076baf9ffb
Ayam62 <153174024+Ayam62@users.noreply.github.com>
2024-10-27 16:36:25 +0000

    memory

---
+++ 01_basics/01memory.js
@@ -0,0 +1,4 @@
+//Stack and heap memory
+/* Stack = copy memory */
+/* Heap =  reference memory */
+/* normal variables are created in stack memory while arrays,strings are created in heap memory(reference)*/
