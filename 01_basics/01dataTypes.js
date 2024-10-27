adffac018ce72380d73c24236996fed993774e97
Ayam62 <153174024+Ayam62@users.noreply.github.com>
2024-10-27 07:36:48 +0000

    datatypes

--- 01_basics/01dataTypes.js
+++ 01_basics/01dataTypes.js
@@ -1,5 +1,12 @@
-var name="ayam"
-var age=19
-var gender="male"
-console.table([typeof name,typeof age,typeof "male"])
+//alert(3+3) gives error becase we are in node woekspace and that syntax is only used in browser because the engine if JS is in the browser
+let name="Ayam"//string
+let age=19//number
+let isLoggedIn=true //bool
+// other datatypes =null
+let state =null//empty for eg you have to send a n=tmperature but still the temp of that place is not found so you define it as
+//null so that the consumer understads that the temperature is null so no any value is given
 
+
+// to  view the type of any given value we have the syntax type of ...
+console.table([typeof "Ayam",typeof 19,typeof true,typeof null])
+// null is of object type and undefined is of undefined type.
