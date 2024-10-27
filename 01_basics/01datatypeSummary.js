43e5e532f38baa02ee847e0927a327ee6a2a7639
Ayam62 <153174024+Ayam62@users.noreply.github.com>
2024-10-27 16:12:02 +0000

    hehe

--- 01_basics/01datatypeSummary.js
+++ 01_basics/01datatypeSummary.js
@@ -2,3 +2,12 @@
 //7 types: string,number,boolean,null,undefined,Symbol,Bigint
 //Non primitive(Reference)
 //Array,Objects,Functions
+const id=Symbol('123')
+const anotherId= Symbol('123')
+//For array.
+
+const heros=["Ayam","Hero","power ranger"];
+let myobj={
+    name :"ayam",//syntax uses semi colon
+    age:19
+}
