b6f72b5237dee9373083cb1d26e36b611aa4c17e
Ayam62 <153174024+Ayam62@users.noreply.github.com>
2024-10-31 16:15:53 +0000

    truthyjs

--- 04_controlFlow/truthy.js
+++ 04_controlFlow/truthy.js
@@ -23,3 +23,18 @@ const emptyObj={}
 if(Object.keys(emptyObj).length===0){// note: Object.keys(objectname) gives an array
     console.log("Obj is empty")
 }
+//Nulllish Coalescing Operator(??).... null undefined
+// let val1;
+// val1=5??10
+// val1=null??10
+// console.log(val1);
+//Note: Hence Nullish Coalescing Operator(??) assigns the right hand operator if null or undefined is int the right hand side otherwise it always assignes left hand operator. If null??10??20 then first value is 10 so 10 remains
+
+
+//TERNARY OPERATOR
+
+let a=5;
+let b=3;
+let c=2
+let g;
+console.log(`${g} is the greater number`)
